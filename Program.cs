24acdac7d9fe1c6f6fa0728dd7bdd1afa908fba0
OVGI <girichevao@gmail.com>
2022-12-17 15:04:03 +0300

    add task_50

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+в двумерном массиве, и возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет         */
+
+int[,] array = new int[3, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+int iu, ju;
+Console.Write($"Введите i: ");
+int.TryParse(Console.ReadLine(), out iu);
+Console.Write($"Введите j: ");
+int.TryParse(Console.ReadLine(), out ju);
+
+if (iu < 0 || iu >= array.GetLength(0))
+    Console.Write($"Неверный индекс строки");
+else if (ju < 0 || ju >= array.GetLength(1))
+    Console.Write($"Неверный индекс столбца");
+else
+    Console.Write($"{array[iu, ju]}");
